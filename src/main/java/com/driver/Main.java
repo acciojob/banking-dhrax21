8a56fc4855e050363b99252e5b0092c24b24770e
Dheeraj Singh <dhrax21bc@gmail.com>
2022-12-30 20:38:22 +0530

    first commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,7 +1,20 @@
 package com.driver;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
+//        SavingsAccount ubi=new SavingsAccount("hp",10000,5000,8);
+//
+//        System.out.println(ubi.getName());
+//        System.out.println(ubi.getMinBalance());
+//        System.out.println(ubi.generateAccountNumber(9,78));
+//        ubi.setMinBalance(200);
+//        ubi.setName("ACER");
+//        System.out.println(ubi.getName());
+//        ubi.deposit(15000);
+//        System.out.println(ubi.getBalance());
+//        System.out.println(ubi.getSimpleInterest(2));
+//        System.out.println(ubi.getCompoundInterest(4,6));
+//        ubi.withdraw(900);
     }
 }
